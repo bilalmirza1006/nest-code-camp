48f1cd0f73be2d49acf79a0f4e3162809ca5fe2d
Bilal Mirza <bilal.tech1006@gmail.com>
2024-09-20 00:34:51 +0500

    more effective the code and efficiancy and seprator the decorator

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { User } from '@prisma/client';
 import { Request } from 'express';
+import { JwtGuard } from 'src/auth/guard';
+import { GetUser } from 'src/decorator';
 
-@UseGuards(AuthGuard('jwt'))
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
     @Get('me')
-    getMe(@Req() req: Request) {
-        return req.user;
+    getMe(@GetUser() user: User,@GetUser('email') email: string) {
+        console.log("email", email);
+        
+        return user;
     }
+
+    @Patch('')
+    editUser() {
+
+    }
+
 }
