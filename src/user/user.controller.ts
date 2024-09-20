659935ad0eeaa39f7bcd7458bf5a3cdbe4da467e
Bilal Mirza <bilal.tech1006@gmail.com>
2024-09-20 21:22:49 +0500

    create the edit user api and add the user test case

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,23 +1,26 @@
-import { Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { User } from '@prisma/client';
 import { Request } from 'express';
-import { JwtGuard } from 'src/auth/guard';
-import { GetUser } from 'src/decorator';
+import { JwtGuard } from '../auth/guard/jwt.guard';
+import { GetUser } from '../decorator';
+import { EditUserDto } from './dto';
+import { UserService } from './user.service';
 
 @UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
+    constructor(private userService: UserService) { }
     @Get('me')
-    getMe(@GetUser() user: User,@GetUser('email') email: string) {
-        console.log("email", email);
-        
+    getMe(@GetUser() user: User, @GetUser('email') email: string) {
+        // console.log("email", ema);
+
         return user;
     }
 
     @Patch('')
-    editUser() {
-
+    editUser(@GetUser('id') userId: number, @Body() dto: EditUserDto) {
+        return this.userService.editUser(userId, dto)
     }
 
 }
